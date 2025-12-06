3a3012db176124dbe82ba820cfadfc45c870681f
yqyayhqhqhhhjjjqj-dotcom <yqyayhqhqhhhjjjqj@gmail.com>
2025-12-06 14:34:59 -0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,50 +1,26 @@
 const express = require("express");
-const path = require("path");
-const { WebSocketServer } = require("ws");
-
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Servir archivos estáticos
-app.use(express.static(path.join(__dirname)));
-
-const server = app.listen(PORT, () => {
-  console.log("Servidor corriendo en puerto " + PORT);
-});
-
-// Servidor WebSocket
-const wss = new WebSocketServer({ server });
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 let waiting = null;
 
-wss.on("connection", (ws) => {
-  console.log("Usuario conectado");
-
-  if (waiting === null) {
-    waiting = ws;
-    ws.send(JSON.stringify({ type: "status", message: "Esperando a otro usuario..." }));
-  } else {
-    let partner = waiting;
-    waiting = null;
-
-    ws.partner = partner;
-    partner.partner = ws;
-
-    ws.send(JSON.stringify({ type: "status", message: "¡Conectado!" }));
-    partner.send(JSON.stringify({ type: "status", message: "¡Conectado!" }));
-  }
-
-  ws.on("message", (msg) => {
-    if (ws.partner && ws.partner.readyState === ws.partner.OPEN) {
-      ws.partner.send(JSON.stringify({ type: "message", message: msg.toString() }));
+io.on("connection", socket => {
+    if (waiting === null) {
+        waiting = socket;
+    } else {
+        waiting.emit("match", socket.id);
+        socket.emit("match", waiting.id);
+        waiting = null;
     }
-  });
 
-  ws.on("close", () => {
-    if (ws.partner) {
-      ws.partner.send(JSON.stringify({ type: "status", message: "El otro usuario se desconectó." }));
-      ws.partner.partner = null;
-    }
-    if (waiting === ws) waiting = null;
-  });
+    socket.on("signal", data => {
+        io.to(data.to).emit("signal", { from: socket.id, signal: data.signal });
+    });
+
+    socket.on("disconnect", () => {
+        if (waiting === socket) waiting = null;
+    });
 });
+
+server.listen(3000, () => console.log("Servidor listo en puerto 3000"));
